72bacbff66a154b84ab4a26f2ac924f044cf37a2
Renny <connect.renny@gmail.com>
2023-02-27 19:56:05 +0530

    Files
    Upload Files

---
+++ includes/nav-sticky.php
@@ -0,0 +1,55 @@
+
+    <div class="navbar-sticky">
+      <span></span>
+      <ul class="social-list">
+        <li class="soc-item">
+          <a
+            type="button"
+            class="soc-link"
+            data-toggle="tooltip"
+            href="#"
+            title="Facebook"
+          >
+            <i class="fa-brands fa-facebook-f"></i>
+          </a>
+        </li>
+        <li class="soc-item">
+          <a
+            type="button"
+            class="soc-link"
+            data-toggle="tooltip"
+            href="#"
+            title="Instagram"
+          >
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+        </li>
+        <li class="soc-item">
+          <a
+            type="button"
+            class="soc-link"
+            data-toggle="tooltip"
+            href="#"
+            title="YouTube"
+          >
+            <i class="fa-brands fa-youtube"></i>
+          </a>
+        </li>
+        <li class="soc-item">
+          <a
+            type="button"
+            class="soc-link"
+            data-toggle="tooltip"
+            href="#"
+            title="Twitter"
+          >
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+        </li>
+      </ul>
+      <a class="nav-whatsapp" href="javascript:;"
+        ><i class="bi bi-whatsapp"></i
+      ></a>
+    </div>
+
+    
